4b245a9814e7e9b59c01703f2124c773f1af4e0a
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-04-18 21:53:35 -0400

    1.23 15 class syntax - object constructor

---
+++ 02 JavaScript Algorithms/15 use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,12 @@
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+/* The constructor method is a special method for creating and initializing an object created with a class. You will learn more about it in the Object Oriented Programming section of the JavaScript Algorithms And Data Structures Certification. */
+
+/* Used the class keyword and wrote a constructor to create the Vegetable class. The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor. */
