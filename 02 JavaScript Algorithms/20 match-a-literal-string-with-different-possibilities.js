e79a208982ecdaecd7aa449fb4acf0c2c4ec357b
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-04-20 13:55:39 -0400

    moving regex files to personal

--- 02 JavaScript Algorithms/20 match-a-literal-string-with-different-possibilities.js
+++
@@ -1,5 +0,0 @@
-let petString = "James has a pet cat.";
-let petRegex = /dog|cat|bird|fish/;
-let result = petRegex.test(petString);
-
-/*The petRegex regular expression is using alternation (|) to match any of the words "dog", "cat", "bird", or "fish". The test() method is then used to check if any part of the petString matches the regular expression, and the resulting boolean value is stored in the result variable.*/
--- 02 JavaScript Algorithms/22 extract-matches.js
+++
@@ -1,5 +0,0 @@
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/;
-let result = extractStr.match(codingRegex);
-
-/*You can also extract the actual matches you found with the .match() method. Note that the .match syntax is the "opposite" of the .test method you have been using thus far. */
