8932e49bc0d8944e13dbfd777dea9d8fa98cd936
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-04-22 19:28:46 -0400

    Debugging folder added

---
+++ 02 JavaScript Algorithms/Debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
@@ -0,0 +1,24 @@
+
+
+
+/*
+The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+*/
+
+
+
+
+//My solution - moving the nested loop outside of the first loop
+function zeroArray(m, n) {
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    newArray.push(row);
+  }
+  for (let j = 0; j < n; j++) {
+      row.push(0);
+    }
+  return newArray;
+}
+let matrix = zeroArray(3, 2);
+console.log(matrix);
