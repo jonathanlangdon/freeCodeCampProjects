c87bb6f390177c05e37b08877db4820053639390
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-04-25 18:17:12 -0400

    iterate through arrays

---
+++ 02 JavaScript Algorithms/Data Structures/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,13 @@
+let myNestedArray = [
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array'],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary'],
+  ['ready for this?',
+    ['deep', 
+      ['deeper',
+        ['deepest']
+      ]
+    ]
+  ]
+];
