4ba4f99e1ec85d516e58834890e03dddbe63638e
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-04-25 18:46:46 -0400

    iterate through keys of objects... for in loop

---
+++ 02 JavaScript Algorithms/Data Structures/iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -0,0 +1,22 @@
+const users = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  let onlineCount = 0;
+  for (let user in usersObj) 
+    usersObj[user].online === true && onlineCount++;
+  return onlineCount;
+}
+
+console.log(countOnline(users));
+
+/* We've defined a function countOnline which accepts one argument (a users object). Use a for...in statement within this function to loop through the users object passed into the function and return the number of users whose online property is set to true. An example of a users object which could be passed to countOnline is shown below. Each user will have an online property with either a true or false value. */
