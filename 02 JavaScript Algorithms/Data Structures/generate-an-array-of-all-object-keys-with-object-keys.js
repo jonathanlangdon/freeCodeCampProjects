652b83a55e6ec9dc3e446a2dc1cb56aedce59d69
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-04-25 18:50:16 -0400

    Object.keys() to get array of all keys in object

---
+++ 02 JavaScript Algorithms/Data Structures/generate-an-array-of-all-object-keys-with-object-keys.js
@@ -0,0 +1,30 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+//My created function below
+const getArrayOfUsers = obj => Object.keys(obj);
+
+console.log(getArrayOfUsers(users));
+
+/* Generate an Array of All Object Keys with Object.keys()
+
+We can also generate an array which contains all the keys stored in an object with the Object.keys() method. This method takes an object as the argument and returns an array of strings representing each property in the object. Again, there will be no specific order to the entries in the array.
+
+Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+*/
