a2a4428cdf223557149772ef8f9f46844b71a5a4
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-01 18:15:47 -0400

    using .reduce()

--- 02 JavaScript Algorithms/functionalProgramming/bash-commit-in-terminal.sh
+++ 02 JavaScript Algorithms/functionalProgramming/bash-commit-in-terminal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get the name of the file to commit
-read -p "Github comment: " filename
+read -p "Github comment: " comment
 git add .
-git commit -m "Committing $filename"
+git commit -m "$comment"
 git push
 sleep 5
