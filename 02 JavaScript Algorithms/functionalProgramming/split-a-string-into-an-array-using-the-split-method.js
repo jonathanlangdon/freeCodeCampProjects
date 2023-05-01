9f223327d1d7d7e0996fafbf910a0ef4e147419a
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-01 18:17:54 -0400

    split string into array challenge

---
+++ 02 JavaScript Algorithms/functionalProgramming/split-a-string-into-an-array-using-the-split-method.js
@@ -0,0 +1,7 @@
+const splitify = str => str.split(/\W/);
+
+console.log(splitify("Hello World,I-am code"));
+
+/*
+Use the split method inside the splitify function to split str into an array of words. The function should return the array. Note that the words are not always separated by spaces, and the array should not contain punctuation.
+*/
