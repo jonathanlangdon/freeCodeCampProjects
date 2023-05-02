e1fbf0741c8ba28a0f8731383af1ee6f9d644134
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-02 16:34:06 -0400

    use-the-some-method-to-check-that-any-elements-in-an-array-meet-a-criteria.js

---
+++ 02 JavaScript Algorithms/functionalProgramming/use-the-some-method-to-check-that-any-elements-in-an-array-meet-a-criteria.js
@@ -0,0 +1,5 @@
+const checkPositive = arr => arr.some(n => n > 0);
+
+checkPositive([1, 2, 3, -4, 5]);
+
+/* Use the some method inside the checkPositive function to check if any element in arr is positive. The function should return a Boolean value. */
