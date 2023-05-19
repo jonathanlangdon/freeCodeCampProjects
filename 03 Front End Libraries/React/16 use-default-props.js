71ed1f731b446be08ef07086b83194c8eea70e11
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-19 18:34:49 -0400

    Use default props

---
+++ 03 Front End Libraries/React/16 use-default-props.js
@@ -0,0 +1,11 @@
+const ShoppingCart = (props) => {
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+
+ShoppingCart.defaultProps = { items: 0}
+
+/* The code editor shows a ShoppingCart component. Define default props on this component which specify a prop items with a value of 0. */
