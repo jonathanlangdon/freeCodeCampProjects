a7926c9e2e6a59c64bac8c2362c7ca3c4a28c13b
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-20 15:57:51 -0400

    React19 access props using this prop

---
+++ 03 Front End Libraries/React/19 access-props-using-this-props.js
@@ -0,0 +1,36 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <Welcome name={"Jonathan"}/>
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          { /* Change code below this line */ }
+          <p>Hello, <strong>{this.props.name}</strong>!</p>
+          { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+
+/*
+Render an instance of the Welcome component in the parent component App. Here, give Welcome a prop of name and assign it a value of a string. Within the child, Welcome, access the name prop within the strong tags.
+*/
