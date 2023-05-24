809be4a3f02059b6382c382461e0b3422e73ced8
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-24 16:53:44 -0400

    React32 Use the lifecylce method component

---
+++ 03 Front End Libraries/React/32 use-the-lifecycle-method-componentdidmount.js
@@ -0,0 +1,26 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+/* There is a mock API call in componentDidMount(). It sets state after 2.5 seconds to simulate calling a server to retrieve data. This example requests the current total active users for a site. In the render method, render the value of activeUsers in the h1 after the text Active Users:. Watch what happens in the preview, and feel free to change the timeout to see the different effects. */
