a2a076c7ddf757bed1113f53816ee8aa04d06c8b
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-25 19:02:29 -0400

    React35 introducing in-line styles

---
+++ 03 Front End Libraries/React/35 introducing-inline-styles.js
@@ -0,0 +1,14 @@
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color: "red", fontSize: 72}}>Big Red</div>
+    );
+  }
+};
+
+/* 
+
+Add a style attribute to the div in the code editor to give the text a color of red and font size of 72px.
+
+Note that you can optionally set the font size to be a number, omitting the units px, or write it as 72px.
+*/
