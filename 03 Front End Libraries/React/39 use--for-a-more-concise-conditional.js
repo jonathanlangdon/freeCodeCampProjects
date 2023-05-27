28eae6b433cb62d34797760fa086d66fef3ead03
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-26 21:11:06 -0400

    React39 use for a more concise conditoinal

---
+++ 03 Front End Libraries/React/39 use--for-a-more-concise-conditional.js
@@ -0,0 +1,27 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+    return (
+        <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         {this.state.display && (
+           <h1>Displayed!</h1>
+          )} 
+        </div>
+    );
+  }
+};
+
+/* Solve the previous example again, so the h1 only renders if display is true, but use the && logical operator instead of an if/else statement. */
