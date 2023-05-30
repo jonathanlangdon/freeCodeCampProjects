afeaae8d34b60ded61bbdb44e2f4e7fcd2f131f2
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-29 21:22:51 -0400

    Redux 01 create a redux store

---
+++ 03 Front End Libraries/Redux/01 create-a-redux-store.js
@@ -0,0 +1,18 @@
+const reducer = (state = 5) => {
+  return state;
+}
+
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+
+const store = Redux.createStore(reducer);
+
+/* 
+
+The Redux store is an object which holds and manages application state. There is a method called createStore() on the Redux object, which you use to create the Redux store. This method takes a reducer function as a required argument. The reducer function is covered in a later challenge, and is already defined for you in the code editor. It simply takes state as an argument and returns state.
+
+Declare a store variable and assign it to the createStore() method, passing in the reducer as an argument.
+
+Note: The code in the editor uses ES6 default argument syntax to initialize this state to hold a value of 5. If you're not familiar with default arguments, you can refer to the ES6 section in the Curriculum which covers this topic.
+*/
