6f8fe503f1b3debd481e8c65db5b06f4ba2363b4
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-05-30 08:22:48 -0400

    Redux04 define action creator

---
+++ 03 Front End Libraries/Redux/04 define-an-action-creator.js
@@ -0,0 +1,8 @@
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+
+const actionCreator = () => action
+
+/* Define a function named actionCreator() that returns the action object when called. */
